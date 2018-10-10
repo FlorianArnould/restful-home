37c2f032216b24bad6adb78c47aee1ccd2cadadf
Florian ARNOULD <florian.arnould@ecole.ensicaen.fr>
2018-10-10 23:33:26 +0200

    return to the first travis config

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sonarqubeScanner = require('sonarqube-scanner');
 gulp.task('default', function(callback) {
   sonarqubeScanner({
     serverUrl : "https://sonarcloud.io",
-    token : "kM3O3WSBSodpfYoZOS9rBWdVHOm0RNyOhsCWzx3k7wE3hJlbGWL42DkndAcWZir5pGxEFDdyeNFcoqULdzMownX8gfN83XG8rDAuSA6XDrFBnLUOFYFm8uIrTRL6Hn3BEaglelXgUisDDgjcg/UrtV5gzB/XloDFhbGgSDxYo4yhwD9a1DLd2hlmC+fgsNXfDd8NzoP/lyAMJRc5YmkRnbGZzdpb42NfIAtnV+lj88FCnxmShATSFYN6ovb+zVj/JYfbCxufAaZDik/2zb8q5B3o/BM6RoYWQX8ssdsM/JU5d5bYgWLyzAizs2p/mGVGlaI56lTx/zySnK/I8f/MKWEbUlVxEAQJl48rHakpEDCBfmGHnKj7PX2TNX0d5SGJiIO75mJsyrsKCic4WvCNiml9oRXKY2hz+Zverb3xffYpbczomp5+Mf0jWvNlHvxD+howNakVvYgIjBAl918nLC3zzK1rAqN4z2GL2epx2+Szw+9ylaKLW1dxWt9jQW5dbaXSXYpIMZKuIBMAg1feSk883pTBEEun52sd7dEMJOTigb8coSPcC2fswxL9sUI4eVKVUhc4VjDw6nQzjQBh6+ILmYbIUYd1Ndl7ySAEC29UAKbZW0L7sRspXF7O7aqHCPv7d91MS7KMZWOyOxS+7OiXFe/3uBPZW51RaMIshHE=",
+    token : "2d5c58bab38596a7bcbac3575929a2d9861ae654",
     options : {
       "sonar.organization": "florianarnould-github"
     }
